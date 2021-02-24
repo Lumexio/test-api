748f6a5d827969c53e55948e0e4930606b603fe0
Piaczio <trotroyu@gmail.com>
2021-02-24 14:52:00 -0700

    feat: campos aceptados  dentro de la base de datos

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -10,4 +10,18 @@ class Categoria extends Model
     protected $table = 'categorias_tbl';
     protected $primaryKey = 'id';
     use HasFactory;
+
+
+
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nombre_categoria',
+        'descripcion_categoria'
+    ];
 }
